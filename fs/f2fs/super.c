a958750d9260c05735c850ecfcc71ceb95a9cbeb
Edgars Cīrulis <edgarsscirulis@gmail.com>
2022-06-26 14:08:52 +0300

    Revert "f2fs: Move to late_initcall"
    This reverts commit c368e6d8df869e29a257f127fba2595921ad7a00.

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -4049,8 +4049,11 @@ static void __exit exit_f2fs_fs(void)
 	f2fs_destroy_trace_ios();
 }
 
-late_initcall(init_f2fs_fs);
-MODULE_SOFTDEP("pre: crc32");
+module_init(init_f2fs_fs)
+module_exit(exit_f2fs_fs)
+
 MODULE_AUTHOR("Samsung Electronics's Praesto Team");
 MODULE_DESCRIPTION("Flash Friendly File System");
 MODULE_LICENSE("GPL");
+MODULE_SOFTDEP("pre: crc32");
+
